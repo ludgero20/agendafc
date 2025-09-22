ca0dbb23476eff4679ec15425fb6660c96655ee9
ludgero20 <lurimussi@gmail.com>
2025-09-22 21:35:54 +0000

    instalaçao facilitada

--- next.config.js
+++ next.config.js
@@ -1,21 +1,34 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'www.thesportsdb.com', // Mantemos o antigo por segurança
-        port: '',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "www.thesportsdb.com",
+        port: "",
+        pathname: "/images/**",
       },
       {
-        protocol: 'https',
-        hostname: 'r2.thesportsdb.com', // ADICIONAMOS O NOVO DOMÍNIO AQUI
-        port: '',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "r2.thesportsdb.com",
+        port: "",
+        pathname: "/images/**",
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
